2920e0dc108ec85bd42e7d3608f0f221e1d3368c
Baptiste Maingret <baptiste.maingret@gmail.com>
2021-12-22 01:39:52 +0100

    first omdel

--- chapter-2/chapter2/data/make_dataset.py
+++ chapter-2/chapter2/data/make_dataset.py
@@ -2,7 +2,6 @@
 import pathlib
 import click
 from dotenv import find_dotenv, load_dotenv
-import os
 import pandas as pd
 from sklearn import model_selection
 from chapter2.conf import datasources, DataDirectory
@@ -11,9 +10,11 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 def _save_path(root_data_dir, data_subdir, name):
     save_path = pathlib.Path(root_data_dir).joinpath(data_subdir).joinpath(name)
     return save_path
+
 
 def save_data(dataframe, root_data_dir, data_subdir: DataDirectory, name):
     save_path = _save_path(root_data_dir, data_subdir.value, name)
@@ -21,23 +22,35 @@ def save_data(dataframe, root_data_dir, data_subdir: DataDirectory, name):
     dataframe.rename_axis("index").to_csv(save_path)
     return
 
+
 def load_data(root_data_dir, data_subdir: DataDirectory, name):
     save_path = _save_path(root_data_dir, data_subdir.value, name)
     return pd.read_csv(save_path)
 
-def train_test_split(dataframe):
-    train, test = model_selection.train_test_split(dataframe, test_size=0.2, random_state=0)
-    return train, test
+
+def train_test_split(housing):
+    housing_y = housing["median_house_value"]
+    housing_X = housing.drop(["median_house_value"], axis=1)
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(
+        housing_X,
+        housing_y,
+        test_size=0.2,
+        random_state=0,
+        stratify=housing["ocean_proximity"],
+    )
+    logger.debug(y_train.shape)
+    logger.debug(X_train.shape)
+    return pd.concat([X_train, y_train], axis=1), pd.concat([X_test, y_test], axis=1)
 
 
 @click.command()
-@click.argument('data_root_dir', type=click.Path(exists=True))
+@click.argument("data_root_dir", type=click.Path(exists=True))
 def make_dataset(data_root_dir):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+    """Runs data processing scripts to turn raw data from (../raw) into
+    cleaned data ready to be analyzed (saved in ../processed).
     """
 
-    logger.info('making final data set from raw data')
+    logger.info("making final data set from raw data")
 
     housing_path = datasources.get_data("housing")
     housing = pd.read_csv(housing_path)
@@ -45,8 +58,8 @@ def make_dataset(data_root_dir):
     save_data(train, data_root_dir, DataDirectory.INTERIM, "housing_train.csv")
     save_data(test, data_root_dir, DataDirectory.INTERIM, "housing_test.csv")
 
-if __name__ == '__main__':
 
+if __name__ == "__main__":
 
     load_dotenv(find_dotenv())
 
